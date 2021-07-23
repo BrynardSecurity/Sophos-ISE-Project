08765b2c996e6f34d11603f85abe4a7baf7e8254
Ralph Brynard <ralph@thebrynards.com>
2021-07-23 19:46:24 -0400

    commit to aws

--- sophos_range_cloud/scripts/windows_deploy_aws.ps1
+++ sophos_range_cloud/scripts/windows_deploy_aws.ps1
@@ -8,14 +8,13 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install the required packages
-$packages = @('python','awscli','git','terraform')
+choco install "python;awscli;git;terraform"
 
-ForEach ($i in $packages){
-    choco install $i -y 
-}
+#clone the repository
+git clone https://github.com/BrynardSecurity/Sophos-ISE-Project && cd Sophos-ISE-Project/sophos_range_cloud
 
 #init terraform modules
-cd terraform\aws\local
+cd ..\terraform\aws\local
 terraform init 
 cd ..\..\..
 
