44c53f40c3912158e327cf15ef4ef8e40eca0914
Ralph Brynard <ralph@thebrynards.com>
2021-07-26 14:33:56 -0400

    commit on main

--- sophos_range_cloud/terraform/aws/modules/splunk-server/variable.tf
+++
@@ -1,5 +0,0 @@
-
-variable "config" { }
-variable "vpc_security_group_ids" { }
-variable "ec2_subnet_id" { }
-variable "phantom_server_instance" { }
--- sophos_range_cloud/terraform/azure/modules/splunk-server/variable.tf
+++
@@ -1,5 +0,0 @@
-
-variable "config" { }
-variable "rg_name" { }
-variable "subnet_id" { }
-variable "phantom_server_instance" { }
